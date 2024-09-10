9219c23510648fb64aef36bfb52dfe57c45fa87a
Anton Deulia <anton.deulia@kauz.net>
2024-09-10 14:45:11 +0200

    set google envs

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -2,14 +2,17 @@ import { Injectable, UnauthorizedException } from "@nestjs/common"
 import { PassportStrategy } from "@nestjs/passport"
 import { AuthService } from "../auth.service"
 import { Profile, Strategy } from "passport-google-oauth20"
+import { ConfigService } from "@nestjs/config"
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-	constructor(private readonly authService: AuthService) {
+	constructor(
+		private readonly authService: AuthService,
+		private readonly configService: ConfigService
+	) {
 		super({
-			clientID:
-				"226634404321-2isqn0hre3mtfetmlvpk2lt6gfb9ccrh.apps.googleusercontent.com",
-			clientSecret: "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",
+			clientID: configService.getOrThrow("GOOGLE_CLIENT_ID"),
+			clientSecret: configService.getOrThrow("GOOGLE_CLIENT_SECRET"),
 			callbackURL: "http://localhost:3000/auth/google/callback",
 			scope: ["email"]
 		})
